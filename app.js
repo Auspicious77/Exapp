1e4d0aac417f4ba3dd8c20b2e7e445ee9ece8758
Auspicious77 <elishaibukun@gmail.com>
2025-02-11 10:01:38 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express')
+
+const app = express();
+
+
+
+app.use((req, res, next) => {
+    console.log("In a middleware")
+
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware")
+
+});
+
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
