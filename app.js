5852a2c54befd5efe5c55d9c95ff24e383226bca
Auspicious77 <elishaibukun@gmail.com>
2025-02-20 11:24:32 +0100

    updated

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const expressHbs = require('express-handlebars')
+
 
 // Use Express's built-in body parser middleware
 app.use(express.urlencoded({ extended: true }));
+
+// app.engine('handlebars', expressHbs());
+// app.set('view engine', 'pug');
+// app.set('views', 'handlebars');
+// app.set('views', 'views');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const errorController = require('./controllers/error')
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,9 +34,12 @@ app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'views', '404.html'))
-})
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, 'views', '404.html'))
+// })
+
+// app.use(errorController.error);
+app.use(errorController.get404);
 
 
 
