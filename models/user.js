7dc45c66fed3f4730462456c1d3889448fc0b176
Auspicious77 <elishaibukun@gmail.com>
2025-04-04 09:51:34 +0100

    Exp-update

--- models/user.js
+++ models/user.js
@@ -51,5 +51,20 @@ userSchema.methods.addToCart = function(product){
     return this.save();
 }
 
+//remove from cart method
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  })
+
+  this.cart.items = updatedCartItems;
+  return this.save();
+}
+
+//clear cart method
+userSchema.methods.clearCart = function(){
+  this.cart = {items: []};
+  return this.save();
+}
 
 module.exports = mongoose.model('User', userSchema);
